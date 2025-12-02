e47fa649836a31836ec8cfc8d44d2694eeb71300
software-boop <balaji@brihaspathi.com>
2025-12-02 15:31:08 +0530

    ejjej

--- app/case-studies/[sectorSlug]/page.tsx
+++ app/case-studies/[sectorSlug]/page.tsx
@@ -1,5 +1,3 @@
-
-export const runtime = "edge";
 "use client";
 
 import React, { useEffect, useState } from "react";
--- app/events/[documentId]/page.tsx
+++ app/events/[documentId]/page.tsx
@@ -1,6 +1,6 @@
 
 
-export const runtime = "edge";
+
 
 import Image from "next/image";
 import ScrollToGalleryButton from "../ScrollToGalleryButton";
