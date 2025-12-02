74964f3389df9606f14d2544878ceb2aeb6da596
software-boop <balaji@brihaspathi.com>
2025-12-02 15:54:37 +0530

    jjjk

--- app/(super-admin)/super-admin/Addprojects/page.tsx
+++ app/(super-admin)/super-admin/Addprojects/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import ProjectsPage from '@/app/projects/page';
 import React, { Suspense } from 'react'
 
-import Onliveproject from "./Projectspage"
+
 function page() {
   return (
     <div>
       <Suspense fallback={<div className="p-4">Loading departments...</div>}>
 
 
-        <Onliveproject />
+        <ProjectsPage />
 
       </Suspense>
 
